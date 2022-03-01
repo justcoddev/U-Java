7b6b8eb2e6fbd3fe2416298ea36481a62e6032c4
justcoddev <37991332+justcoddev@users.noreply.github.com>
2022-03-01 14:16:57 -0500

    5. Ejercicio: Hola mundo con Apache
    5.  
    Universidad Java - De Cero a Experto - Más Completo +106 hrs
    
    https://www.udemy.com/share/101Wlw3@GgD4mzl_9p8kA1Yr8RTqEhPoQlPeQcpaz5gw9mbnyZ4uk2AtYDJvc4ItDCh1izmH/

---
+++ Fundamentos/HolaMundoJava/src/HolaMundo.java
@@ -0,0 +1,12 @@
+
+/**
+ *
+ * @author JUSTCODDEV
+ */
+//Mi clase en Java
+public class HolaMundo {
+
+    public static void main(String args[]) {
+        System.out.println("Hola Mundo");
+    }
+}
