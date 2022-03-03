28fff89fc18c47b07b204de8ec3ddf66319782f7
justcoddev <37991332+justcoddev@users.noreply.github.com>
2022-03-02 20:15:42 -0500

    _002_VariablesTiposdeEnteros
    Tipos de datos
    int
    String
    var

---
+++ Fundamentos/_002_VariablesTiposdeEnteros/src/_002_VariablesTiposdeEnteros/VariablesTiposdeEnteros.java
@@ -0,0 +1,49 @@
+package _002_VariablesTiposdeEnteros;
+
+/**
+ *
+ * @author JUSTCODDEV
+ */
+public class VariablesTiposdeEnteros {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Definir la variable
+        int miVariableEntera = 10;
+        System.out.println(miVariableEntera);
+        //Modificar el valor de la variable
+        miVariableEntera = 5;
+        System.out.println(miVariableEntera);
+
+        //Tipo String
+        String miVariableCadena = "Hola";
+        System.out.println(miVariableCadena);
+        //Modificar el valor de la variable
+        miVariableCadena = "Bye";
+        System.out.println(miVariableCadena);
+
+        //var - Inferencia de tipos en Java
+        var miVariableEntera2 = 15;
+        System.out.println(miVariableEntera2);
+
+        //var - Inferencia de tipos en Java String
+        var miVariableCadena2 = "String con var";
+        System.out.println("miVariableCadena2 = " + miVariableCadena2);
+        
+        //Valores permitidos en el nombre de variables
+        var miVarialbe = 1;
+        var _mivariable = 2;
+        var $miVariable = 3;
+        //var áVariable = 10; no se recomienda utilizar
+        //var #miVariable = 2; no se permite utilizar caraceres especiales
+        
+        
+        
+        
+        
+        
+        //Atajos: sout - soutv
+    }
+}
