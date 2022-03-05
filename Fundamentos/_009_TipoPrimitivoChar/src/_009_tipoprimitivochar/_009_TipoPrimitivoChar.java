721a0f21ecbef185efc314a6d2e834e4193137d6
justcoddev <37991332+justcoddev@users.noreply.github.com>
2022-03-05 12:01:08 -0500

    _009_TipoPrimitivoChar

---
+++ Fundamentos/_009_TipoPrimitivoChar/src/_009_tipoprimitivochar/_009_TipoPrimitivoChar.java
@@ -0,0 +1,40 @@
+package _009_tipoprimitivochar;
+
+/**
+ *
+ * @author JUSTCODDEV
+ */
+public class _009_TipoPrimitivoChar {
+
+    public static void main(String[] args) {
+        char miCaracter = 'a';
+        System.out.println("miCaracter = " + miCaracter);
+        
+        char varChar = '\u0021';
+        System.out.println("varChar = " + varChar);
+        
+        char varCharDecimal = 33;
+        System.out.println("varCharDecimal = " + varCharDecimal);
+        
+        char varCharSimbolo = '!';
+        System.out.println("varCharSimbolo = " + varCharSimbolo);
+        /*
+        con var
+        */
+        var varChar1 = '\u0021';
+        System.out.println("varChar1 = " + varChar1);
+        
+        var varCharDecimal2 = 33;
+        System.out.println("varCharDecimal2 = " + varCharDecimal2);
+        
+        var varCharSimbolo3 = '!';
+        System.out.println("varCharSimbolo3 = " + varCharSimbolo3);
+        
+        int variableEnteraSimbolo = '!';
+        System.out.println("variableEnteraSimbolo = " + variableEnteraSimbolo);
+        
+        int letra = 'A';
+        System.out.println("letra = " + letra);
+    }
+
+}
