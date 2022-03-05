9203ef846a34fb1c59bade4766dfa42c6cbc0d69
justcoddev <37991332+justcoddev@users.noreply.github.com>
2022-03-05 12:00:46 -0500

    _008_MasUsosDeVar

---
+++ Fundamentos/_008_MasUsosDeVar/src/_008_masusosdevar/_008_MasUsosDeVar.java
@@ -0,0 +1,20 @@
+package _008_masusosdevar;
+
+/**
+ *
+ * @author JUSTCODDEV
+ */
+public class _008_MasUsosDeVar {
+
+    public static void main(String[] args) {
+        var numeroEntero = 10;
+        System.out.println("numeroEntero = " + numeroEntero);
+        
+        var numeroDouble = 10.0;
+        System.out.println("numeroDouble = " + numeroDouble);
+        
+        var numeroFloat = 10.0F;
+        System.out.println("numeroFloat = " + numeroFloat);
+    }
+
+}
